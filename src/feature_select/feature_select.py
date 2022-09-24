2c3d61f86f5818866298c0505fcd117028bf3e48
shihao28 <shihao28@gmail.com>
2022-09-24 22:03:30 +0800

    Edited feature selection module

--- src/feature_select/feature_select.py
+++ src/feature_select/feature_select.py
@@ -4,25 +4,29 @@ from sklearn.feature_selection import *
 class FeatureSelect:
     def __init__(
         self, data, label, numeric_features_names,
-        category_features_names):
+        category_features_names, shifted_numeric_features_names,
+        shifted_category_features_names):
 
-        # Shift features
-        mask = ~data.columns.isin([label])
-        data.loc[:, mask] = data.loc[:, mask].shift(1)
-        self.data = data.iloc[1:, ]
-
+        self.data = data
         self.label = label
         self.numeric_features_names = numeric_features_names
         self.category_features_names = category_features_names
+        self.shifted_numeric_features_names = shifted_numeric_features_names
+        self.shifted_category_features_names = shifted_category_features_names
 
         # Get numeric and category features
-        self.numeric_features = self.data[self.numeric_features_names]
-        self.category_features = self.data[self.category_features_names]
+        self.numeric_features = data[self.numeric_features_names]
+        self.category_features = data[self.category_features_names]
+        self.shifted_numeric_features = self.data[self.shifted_numeric_features_names]
+        self.shifted_category_features = self.data[self.shifted_category_features_names]
 
     def select_features(self):
         f_stat, f_stat_pvalues = f_regression(
-            self.data[self.numeric_features_names + self.category_features_names], self.data[self.label])
-        f_stat_pvalues = {column_name:pvalue for column_name, pvalue in zip(self.numeric_features_names + self.category_features_names, f_stat_pvalues)}
+            self.data[
+                self.numeric_features_names + self.category_features_names +\
+                    self.shifted_numeric_features_names + self.shifted_category_features_names],
+            self.data[self.label])
+        f_stat_pvalues = {column_name:pvalue for column_name, pvalue in zip(self.numeric_features_names + self.category_features_names + self.shifted_numeric_features_names + self.shifted_category_features_names, f_stat_pvalues)}
         f_stat_pvalues = {k: f_stat_pvalues[k] for k in sorted(f_stat_pvalues, key=f_stat_pvalues.get, reverse=False)}
- 
+
         return f_stat_pvalues
