84a05e5b8dcc3080b9517397fd6b71712628a295
shihao28 <shihao28@gmail.com>
2022-09-24 22:03:59 +0800

    Edited dl config

--- config/config_dl.py
+++ config/config_dl.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from torch import nn, optim
+
 from src.model_dl import *
 
 
@@ -11,37 +13,61 @@ class ConfigDL(object):
         device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
 
         data=dict(
-            data_path="data/01_raw/RestaurantVisitors.csv",
-            label="total",
+            data_path="data/03_primary/nearby_prices.csv",
+            label="CBOT.ZS_Settle_nearby",
+
             # Only list the features that are to be used
             numeric_features=[
-                "rest1", "rest2", "rest3", "rest4",
+                'CBOT.ZC_Settle_nearby',
+                'Month_sin', 'Month_cos',
+                'Day_sin', 'Day_cos',
+                'Day_of_week_sin', 'Day_of_week_cos',
+                'Week_no_sin', 'Week_no_cos',
             ],
             category_features=[
-                "holiday",
+                # 'Year',
+                'Month',
+                'Day',
+                'Day_of_week',
+                'Week_no',
             ],
             datetime_features=dict(
                 # key is column name, value is datetime format
-                date="%m/%d/%Y",
+                # strptime documentation
+                # https://docs.python.org/3/library/datetime.html
+                date="%Y-%m-%d",
             ),
-            freq="D"
+            freq="D",
+            shift_numeric_features={
+                'CBOT.ZC_Settle_nearby': np.arange(60) + 1,
+            },
+            shift_category_features={
+
+            },
         ),
 
         train_val_test_split=dict(
-            split_ratio=0.3,
+            split_ratio=0.1,
         ),
 
         model={
             # n_out in model =n_steps_out
-            RNN.__name__: RNN(2, 64, 1, 1),
+            RNN.__name__: RNN(4, 64, 1, 1),
         },
 
         model_setting={
-            "n_steps_in": 4,
+            "n_steps_in": 40,
             "n_steps_out": 1,
-            "exog_var": ['holiday'],  # Set it to None if no exog_var
-            # use_seasonal=True,  # whether to use sarima
-            # seasonal_period=7,  # for sarimax
+            # Set it to empty list if no exog_var
+            "exog_var": dict(
+                numeric=[
+                    'Month_sin', 'Month_cos',
+                    'CBOT.ZC_Settle_nearby_shift1'
+                ],
+                category=[
+                    'Month'
+                ]
+            ),
         },
 
         criterion=nn.MSELoss(),
@@ -53,7 +79,7 @@ class ConfigDL(object):
         optimizer=dict(
             alg=optim.SGD,
             param=dict(
-                lr=0.01, momentum=0.9, weight_decay=0.0005
+                lr=0.001, momentum=0.9, weight_decay=0.0005
             )
         ),
 
@@ -81,7 +107,7 @@ class ConfigDL(object):
             artifact_uri="./mlruns/",
             experiment_name="Best Pipeline",
             run_name="trial",
-            registered_model_name="my_cls_model",
+            registered_model_name="my_tsa_model",
             port="5000",
         ),
 
